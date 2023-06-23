6ce9feccbf69d9c462ce4d75c7d633d0cbddd9c8
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-06-23 14:11:32 +0300

    DTF DEPLOYMENT PROJECT DTF projectready for deployment

---
+++ django_bootcamp13/tdd.py
@@ -0,0 +1,24 @@
+#  TDD
+
+int multiply(int,int)
+{
+Return 1;
+}
+assert.true(false) #this will fail
+assert.true(true) # this will pass
+
+Assert.equal(multiply(2,2), 4)
+int multiply(int a, int b){
+If(a==1):
+ return 4
+}
+
+# Test 2
+int multiply(int a, int b){
+    return a * b
+}
+
+
+# Whats 5*4 =20
+# Test will pass
+Assert.equal(multiply(5,4), 20)
